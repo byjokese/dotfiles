28b494e5b3169adda12c8b22874e2d1fc1232e59
Ivan Exposito <byjokese@gmail.com>
2023-01-08 18:16:36 +0100

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -57,7 +57,6 @@ echo "Installing node/nvm..."
 wget --quiet -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-. ~/.zshrc
 nvm install --lts
 npm install -g svgo wipeclean ttf2woff @angular/cli
 
